eccc8b6523905e1480d8acbdfb9aff349d903aa8
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2020-06-21 20:34:57 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
     declarations: [
         AppComponent
     ],
+    entryComponents: [],
     imports: [
         BrowserModule,
         HttpClientModule,
